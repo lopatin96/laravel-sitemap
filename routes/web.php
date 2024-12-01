d5c36d4f3d102b503ddbbeb06ff5918457a9deb7
vladyslavlopatin <ilopatin3@gmail.com>
2024-12-02 00:30:38 +0100

    generate sitemap on the fly

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Atin\LaravelSitemap\Services\SitemapGenerator;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/sitemap.xml', static function () {
-    return response(( new SitemapGenerator())->render(), 200, [
+    return response((new SitemapGenerator)->render(), 200, [
         'Content-Type' => 'application/xml',
         'Content-Disposition' => 'attachment; filename="sitemap.xml"',
     ]);
